73f21749f15430d9c677cf1091bca7ec251379bb
jose.ayala <jose.ayala@babelgroup.com>
2024-05-08 09:38:12 +0200

    Fix servicios

--- src/main/java/com/example/unittestdemo/servicios/IServicioLoteria.java
+++ src/main/java/com/example/unittestdemo/servicios/IServicioLoteria.java
@@ -5,5 +5,5 @@ import com.example.unittestdemo.entidades.Bombo;
 import java.util.List;
 
 public interface IServicioLoteria {
-    List<Bombo> listarGanadores(int ctdGanadores);
+
 }
--- src/main/java/com/example/unittestdemo/servicios/ServicioLoteria.java
+++ src/main/java/com/example/unittestdemo/servicios/ServicioLoteria.java
@@ -1,19 +1,10 @@
 package com.example.unittestdemo.servicios;
 
 import com.example.unittestdemo.entidades.Bombo;
-import com.example.unittestdemo.repositorios.IFakeDBLoteria;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ServicioLoteria implements IServicioLoteria {
 
-    private final IFakeDBLoteria iFakeDBLoteria;
-
-    public ServicioLoteria(IFakeDBLoteria iFakeDBLoteria) { this.iFakeDBLoteria = iFakeDBLoteria; }
-
-    @Override
-    public List<Bombo> listarGanadores(int ctdGanadores) {
-
-    }
 }
