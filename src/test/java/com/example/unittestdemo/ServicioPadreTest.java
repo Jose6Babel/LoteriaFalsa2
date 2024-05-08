606c9d6e3da7b023472e7616f63351db19896815
maría.sisamon <maria.sisamon@babelgroup.com>
2024-05-08 11:51:19 +0200

    test1

--- src/test/java/com/example/unittestdemo/ServicioPadreTest.java
+++ src/test/java/com/example/unittestdemo/ServicioPadreTest.java
@@ -29,76 +29,49 @@ public class ServicioPadreTest {
 	IServicioLoteria servicioLoteria;
 
 
-
 	@BeforeAll
 	 void beforeAll(){
 		MockitoAnnotations.openMocks(this);
-	}
-
-
-	@Test
-	public void whenAllLowerCaseAnd5CharsThenReturnObjetoDatosWith1AndPssw(){
-		// Given
-		String pssw = "tests";
-		Mockito.when(servicioPassword.testPsswd(Mockito.anyString())).thenReturn(1);
-
-		ObjetoDatos expected = new ObjetoDatos(1, "tests");
-
-		// When
-		ObjetoDatos response = servicioPadre.metodoPadre(pssw);
-
-		// Then
-		Assertions.assertEquals(expected, response);
 	}
 
 	@Test
 	public void whenPorcentajeIndicadoGeneraNumeroThenReturn(){}
 
 	@Test
-	public void whenIntroduzcoNumeroListaGanadoresThenReturnListaGanadores() {
+	public void whenIntroduzcoNumeroThenReturnListaGanadoresSizeNumero() {
 		// Given
 		Integer numResutlados = 6;
 
-		List<Integer> expectedResult = new ArrayList<>();
-		expectedResult.add(00001);
-		expectedResult.add(00002);
-		expectedResult.add(00003);
-		expectedResult.add(00004);
-		expectedResult.add(00005);
-		expectedResult.add(00006);
-
-
-//		Mockito.when(servicioLoteria.testCorrectoNumeroAndSize(Mockito.anyList())).thenReturn(true);
-
-//		Bombo expected = new Bombo(testBombo);
-
+		List<String> expectedResult = new ArrayList<>();
+		expectedResult.add("00001");
+		expectedResult.add("00002");
+		expectedResult.add("00003");
+		expectedResult.add("00004");
+		expectedResult.add("00005");
+		expectedResult.add("00006");
 
 		// When
-		// servioLoteria.introducirGanador(00001);
-		// servioLoteria.introducirGanador(00002);
-		// servioLoteria.introducirGanador(00003);
-		// servioLoteria.introducirGanador(00004);
-		// servioLoteria.introducirGanador(00005);
-		// servioLoteria.introducirGanador(00006);
-		// List<Integer> listaRes = servicioLoteria.obtenerListadoGanadores(numResultados);
+
+		List<String> mockitoGanadores = new ArrayList<>();
+		expectedResult.add("00000");
+		expectedResult.add("00000");
+		expectedResult.add("00000");
+		expectedResult.add("00000");
+		expectedResult.add("00000");
+		expectedResult.add("00000");
+		expectedResult.add("00001");
+		expectedResult.add("00002");
+		expectedResult.add("00003");
+		expectedResult.add("00004");
+		expectedResult.add("00005");
+		expectedResult.add("00006");
+
+		Mockito.when(servicioLoteria.getListaGanadores().thenReturn(mockitoGanadores));
+
+		List<Integer> listaRes = servicioLoteria.obtenerListadoGanadores(numResultados);
 
 		// Then
 		Assertions.assertEquals(expectedResult, listaRes);
-	}
-
-	@Test
-	public void whenPssIs99ReturnObjectWith99(){
-		// Given
-		String pssw = "tests";
-		Mockito.when(servicioPassword.testPsswd(Mockito.anyString())).thenReturn(99);
-
-		ObjetoDatos expected = new ObjetoDatos(99, "tests");
-
-		// When
-		ObjetoDatos response = servicioPadre.metodoPadre(pssw);
-
-		// Then
-		Assertions.assertEquals(expected, response);
 	}
 
 
